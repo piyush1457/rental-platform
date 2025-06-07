5dab9dc0e3c123d7af2e91f6791da3a8e10becce
Piyush Kumar Sodhi <piyushsodhi145@gmail.com>
2025-06-07 15:32:23 +0530

    Update rental.js

--- server/routes/rental.js
+++ server/routes/rental.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Rental = require('../models/Rental');
 
-// Cancel a rental
+
 router.patch('/:rentalId/cancel', async (req, res) => {
   const { rentalId } = req.params;
 
