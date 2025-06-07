7cce0876a4af70addf8ea04b736b084e33aab5d2
Piyush Kumar Sodhi <piyushsodhi145@gmail.com>
2025-06-07 15:31:17 +0530

    Update checkUser.js

--- server/models/checkUser.js
+++ server/models/checkUser.js
@@ -1,7 +1,7 @@
-// checkUsers.js
+
 const mongoose = require('mongoose');
 require('dotenv').config();
-const User = require('../User'); // Your user model
+const User = require('../User'); 
 
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/rentalPlatform', {
   useNewUrlParser: true,
