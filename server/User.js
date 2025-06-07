22ecec484b4c68168bb2e6390a1420a13fba98ff
Piyush Kumar Sodhi <piyushsodhi145@gmail.com>
2025-06-07 15:27:02 +0530

    Update User.js

--- server/User.js
+++ server/User.js
@@ -6,9 +6,9 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-// Pre-save hook to hash password before saving
+
 userSchema.pre('save', async function (next) {
-  // Only hash if the password is new or modified
+
   if (!this.isModified('password')) return next();
 
   try {
